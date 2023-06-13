d39af5b35627039c53ace690a5cfe184f8344ea0
nishu-09 <nishukumari5005@gmail.com>
2023-06-13 18:28:34 +0530

    error fix

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -22,13 +22,7 @@ const Footer = () => {
                     <li className="menuItem">FAQ</li>
                 </ul>
                 <div className="infoText">
-                    {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. */}
-                    Movix App Clone made by - Nishu and Hrithik
+                    Project By - Nishu 
                 </div>
                 <div className="socialIcons">
                     <span className="icon">
